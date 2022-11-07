5c5d274512ff8e440d086252ecf80d3921986a85
yibe <yibe@DESKTOP-VGVE761>
2022-11-06 23:07:20 -0800

    Changes project compliance to 11

---
+++ my.second.project/src/test/java/NewTest.java
@@ -0,0 +1,15 @@
+import org.testng.annotations.Test;
+
+public class NewTest {
+  @Test
+  public void firstTestNGTest() {
+  }
+  @Test
+  public void canUseVar() {
+	  String myString = "Myname";
+	  
+	  //var is perfect for return type or variables.
+	  var mystring1 = "other name";
+  }
+  
+}
